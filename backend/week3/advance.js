be27c98157bdb4c305f762cf9ab40f9ede9bc6a5
Adarsha Soti <aadarshasoti01@gmail.com>
2025-11-25 11:11:19 +1100

    grid layout and media query

---
+++ backend/week3/advance.js
@@ -0,0 +1,21 @@
+console.log("start");
+
+// setTimeout(()=>{
+//     console.log("set time");
+// },1000);
+
+
+// let count=0;
+// const timer=setInterval(() => {
+//     console.log(count++);
+
+//     if (count==5){
+//         clearInterval(timer);
+//     }
+// }, 1000);
+
+// replica of oops 
+const obj={
+
+};
+console.log("end");
